9a5a2c13db2404a9dd0a413dd61e9353cc18c7d5
Supermasita <francisco@mascia.com.ar>
2022-11-21 00:05:43 +0000

    First test

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # rust-toy-ca
+
+
+## Limitations
+- Serial is always `1`
+- Only ED25519/EdDSA
+- No V3 extensions
+- Input/Output of certs/csr in base64
+
+
+
+## Cool reading
+
+https://blog.pinterjann.is/ed25519-certificates.html
+https://github.com/seanmonstar/warp/tree/master/examples
+https://blog.logrocket.com/creating-a-rest-api-in-rust-with-warp/
+https://learn.microsoft.com/en-us/azure/iot-hub/tutorial-x509-openssl
+https://github.com/Azure/azure-iot-sdk-c/blob/main/tools/CACertificates/certGen.sh
+https://www.digicert.com/kb/ssl-support/openssl-quick-reference-guide.htm
---
+++ src/main.rs
@@ -0,0 +1,132 @@
+use warp::Filter;
+
+use base64::decode;
+use openssl::asn1::{Asn1Integer, Asn1Time};
+use openssl::bn::BigNum;
+use openssl::hash::MessageDigest;
+use openssl::pkey::{PKey, Private};
+use openssl::x509::extension::SubjectKeyIdentifier;
+use openssl::x509::{X509Builder, X509Req, X509};
+use serde_derive::{Deserialize, Serialize};
+use std::fs;
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// CA pub cert
+   #[arg(long)]
+   ca_cert_file: String,
+
+   /// CA priv key
+   #[arg(long)]
+   ca_pkey_file: String,
+}
+
+
+#[derive(Deserialize, Serialize)]
+struct SignReq {
+    csr_base64: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct SignRep {
+    ca_cert_base64: String,
+    signed_cert_base64: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct CaRep {
+    ca_cert_base64: String,
+}
+
+fn _load_ca_cert() -> String {
+    let file_contents: String = fs::read_to_string("test_helpers/ca.crt").expect("Couldn´t read the file");
+    file_contents
+}
+
+fn _load_ca_pk() -> String {
+    let file_contents: String = fs::read_to_string("test_helpers/ca.key").expect("Couldn´t read the file");
+    file_contents
+}
+
+fn _encode_it(data: &[u8]) -> String {
+    return base64::encode(data);
+}
+
+fn _sign_csr_cert(csr_base64: &String, ca_cert: &X509, ca_pkey: &PKey<Private>) -> String {
+    let csr_as_vec = decode(csr_base64).unwrap();
+    let csr = X509Req::from_pem(&csr_as_vec).unwrap();
+    println!("CSR CN {:#?}", &csr.subject_name());
+
+    let one_year = Asn1Time::days_from_now(365).unwrap();
+    let now = Asn1Time::days_from_now(0).unwrap();
+
+    let mut cert_builder = X509Builder::new().unwrap();
+    let subject_key_identifier = SubjectKeyIdentifier::new()
+        .build(&cert_builder.x509v3_context(None, None))
+        .unwrap();
+    cert_builder
+        .append_extension(subject_key_identifier)
+        .unwrap();
+    cert_builder.set_serial_number(&Asn1Integer::from_bn(&BigNum::from_u32(1).unwrap()).unwrap());
+    cert_builder.set_not_before(&now);
+    cert_builder.set_not_after(&one_year);
+    cert_builder.set_issuer_name(&ca_cert.issuer_name());
+    cert_builder.set_subject_name(&csr.subject_name());
+    cert_builder.set_pubkey(&csr.public_key().unwrap());
+    cert_builder.set_version(csr.version().clone());
+
+    // > The openssl docs mention that ed25519 did not support a digest, so openssl requires 
+    // > to pass a NULL ptr as digest algorithm argument. Ref: 
+    // > https://github.com/openssl/openssl/blob/master/doc/man1/openssl-ca.pod.in#L204
+    // (source: https://github.com/sfackler/rust-openssl/issues/1197 )
+    unsafe {
+        cert_builder.sign(&ca_pkey, MessageDigest::from_ptr(std::ptr::null()));
+    };
+    let cert_final = cert_builder.build();
+    println!("CERT FINAL {:#?}", cert_final);
+    return _encode_it(&cert_final.to_pem().unwrap());
+}
+
+async fn _create_from_csr(
+    item: SignReq,
+    ca_cert: X509,
+    ca_pkey: PKey<Private>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let cert_rep = SignRep {
+        ca_cert_base64: base64::encode(_load_ca_cert()),
+        signed_cert_base64: _sign_csr_cert(&item.csr_base64, &ca_cert, &ca_pkey),
+    };
+    Ok(warp::reply::json(&cert_rep))
+}
+
+#[tokio::main]
+async fn main() {
+
+    let args = Args::parse();
+    println!("{:?}", args);
+
+    let ca_cert = X509::from_pem(_load_ca_cert().as_bytes()).unwrap();
+    let ca_cert_w = warp::any().map(move || ca_cert.clone());
+    let ca_pkey = PKey::private_key_from_pem(_load_ca_pk().as_bytes()).unwrap();
+    let ca_pkey_w = warp::any().map(move || ca_pkey.clone());
+
+    // todo: response needs to be json
+    let route_get_ca_cert = warp::path!("get-ca-cert").map(|| base64::encode(_load_ca_cert()));
+
+    let route_create_from_csr = warp::post()
+        .and(warp::path("create-from-csr"))
+        .and(warp::path::end())
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(ca_cert_w.clone())
+        .and(ca_pkey_w.clone())
+        .and_then(_create_from_csr);
+
+    let routes = warp::any().and(route_get_ca_cert.or(route_create_from_csr));
+
+    println!("Running...");
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
