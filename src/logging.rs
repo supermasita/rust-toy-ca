a4a44508ac019286823a86f7a6338468505cfbbf
Supermasita <675228+supermasita@users.noreply.github.com>
2022-11-30 09:20:48 +0000

    Formatting and README update (#5)
    - Run `rustfmt`
    - Update README
      - improve examples
      - fix typo

--- src/logging.rs
+++ src/logging.rs
@@ -2,20 +2,22 @@ use log::LevelFilter;
 
 use log4rs::append::console::ConsoleAppender;
 use log4rs::config::{Appender, Root};
-use log4rs::Config;
 use log4rs::encode::pattern::PatternEncoder;
+use log4rs::Config;
 
 /// log4rs config generator
 pub fn get_log_config() -> log4rs::Config {
     // Returns log4rs configuration
-    
+
     let log_line_pattern = "{h({d(%Y-%m-%d %H:%M:%S)(utc)} - {l}: {m}{n})}";
-    
-    let stdout = ConsoleAppender::builder().encoder(Box::new(PatternEncoder::new(log_line_pattern))).build();
-    
+
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(log_line_pattern)))
+        .build();
+
     let config = Config::builder()
-    .appender(Appender::builder().build("stdout", Box::new(stdout)))
-    .build(Root::builder().appender("stdout").build(LevelFilter::Info))
-    .unwrap();
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .build(Root::builder().appender("stdout").build(LevelFilter::Info))
+        .unwrap();
     return config;
 }
