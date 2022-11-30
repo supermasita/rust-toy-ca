80d4a55aa286cb1e9a540359991dc041c9bb679d
Supermasita <francisco@mascia.com.ar>
2022-11-30 09:08:30 +0000

    Formatting and README update
    - Run `rustfmt`
    - Update README
      - improve examples
      - fix typo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use warp::Filter;
 use base64::decode;
+use clap::Parser;
+use log::{debug, error, info, trace, warn};
+use log4rs;
 use openssl::asn1::{Asn1Integer, Asn1Time};
 use openssl::bn::BigNum;
 use openssl::hash::MessageDigest;
@@ -7,17 +9,15 @@ use openssl::pkey::{PKey, Private};
 use openssl::x509::extension::SubjectKeyIdentifier;
 use openssl::x509::{X509Builder, X509Req, X509};
 use serde_derive::{Deserialize, Serialize};
-use std::fs;
-use clap::Parser;
 use std::fmt;
-use log::{debug, error, info, trace, warn};
-use log4rs;
+use std::fs;
+use warp::Filter;
 mod logging;
 use std::net::SocketAddr;
 
 /// Request status (CA cert request, CSR signing)
 #[derive(Debug, Deserialize, Serialize)]
-enum Status{
+enum Status {
     SUCCESS,
     FAILURE,
 }
@@ -32,17 +32,17 @@ impl fmt::Display for Status {
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-   /// CA public certificate path on storage
-   #[arg(long)]
-   ca_cert_file: String,
+    /// CA public certificate path on storage
+    #[arg(long)]
+    ca_cert_file: String,
 
-   /// CA private key path on storage
-   #[arg(long)]
-   ca_pkey_file: String,
+    /// CA private key path on storage
+    #[arg(long)]
+    ca_pkey_file: String,
 
-   /// IP:Port for the server to listen
-   #[arg(long)]
-   listen: String,
+    /// IP:Port for the server to listen
+    #[arg(long)]
+    listen: String,
 }
 
 /// Signing request
@@ -54,13 +54,13 @@ struct SignReq {
 
 /// Signing request reply
 #[derive(Deserialize, Serialize)]
-struct SignRep { 
+struct SignRep {
     /// the signed certificate as a PEM, base64 encoded
     signed_cert_base64: String,
     /// status
     status: Status,
     /// status message, which can contain the exception message, if there was one
-    status_message: String
+    status_message: String,
 }
 
 /// CA certificate reply
@@ -71,56 +71,62 @@ struct CaCertRep {
     /// status
     status: Status,
     /// status message, which can contain the exception message, if there was one
-    status_message: String
+    status_message: String,
 }
 
 /// Loads the CA public certificate (PEM) from storage
-/// 
+///
 /// # Arguments
 /// * `path`: path for certificate on storage
 fn load_ca_cert(path: &String) -> String {
-    let file_contents: String = fs::read_to_string(path).expect("Couldn´t read the CA certificate file");
+    let file_contents: String =
+        fs::read_to_string(path).expect("Couldn´t read the CA certificate file");
     file_contents
 }
 
 /// Loads the CA private key (PKCS8) from storage
-/// 
+///
 /// # Arguments
 /// * `path`: path for private key on storage
 fn load_ca_pk(path: &String) -> String {
-    let file_contents: String = fs::read_to_string(path).expect("Couldn´t read the CA private key file");
+    let file_contents: String =
+        fs::read_to_string(path).expect("Couldn´t read the CA private key file");
     file_contents
 }
 
 /// Returns base64 encode (String) from passed data
-/// 
+///
 /// # Arguments
-/// * `data`: 
+/// * `data`:
 fn to_base64(data: &[u8]) -> String {
     return base64::encode(data);
 }
 
 /// Returns a public certificate (PEM) created using the passed CSR
-/// 
+///
 /// # Arguments
 /// * `csr_base64`: base64 representation of CSR
 /// * `ca_cert`
 /// * `ca_pkey`
-/// 
+///
 /// # Returns
 /// * PEM certificate (String)
-/// * Status (Status) 
+/// * Status (Status)
 /// * Status Message (String)
-fn create_cert_from_csr(csr_base64: &String, ca_cert: &X509, ca_pkey: &PKey<Private>) -> (String, Status, String) {
+fn create_cert_from_csr(
+    csr_base64: &String,
+    ca_cert: &X509,
+    ca_pkey: &PKey<Private>,
+) -> (String, Status, String) {
     // Decode CSR
-    let csr_as_vec = match decode(csr_base64){
-        Ok(c) =>  c,
-        Err(e) => return ("".to_string(), Status::FAILURE, e.to_string())
-    }; 
-    // Extract CSR from PEM
-    let csr = match X509Req::from_pem(&csr_as_vec){
+    let csr_as_vec = match decode(csr_base64) {
         Ok(c) => c,
-        Err(e) => return ("".to_string(), Status::FAILURE, e.to_string())
+        Err(e) => return ("".to_string(), Status::FAILURE, e.to_string()),
+    };
+    // Extract CSR from PEM
+    let csr = match X509Req::from_pem(&csr_as_vec) {
+        Ok(c) => c,
+        Err(e) => return ("".to_string(), Status::FAILURE, e.to_string()),
     };
 
     // info!("CSR CN {:#?}", &csr.subject_name());
@@ -147,8 +153,8 @@ fn create_cert_from_csr(csr_base64: &String, ca_cert: &X509, ca_pkey: &PKey<Priv
 
     // Sign certificate
     // NOTE:
-    //  > The openssl docs mention that ed25519 did not support a digest, so openssl requires 
-    //  > to pass a NULL ptr as digest algorithm argument. Ref: 
+    //  > The openssl docs mention that ed25519 did not support a digest, so openssl requires
+    //  > to pass a NULL ptr as digest algorithm argument. Ref:
     //  > https://github.com/openssl/openssl/blob/master/doc/man1/openssl-ca.pod.in#L204
     //  (source: https://github.com/sfackler/rust-openssl/issues/1197 )
     unsafe {
@@ -156,17 +162,21 @@ fn create_cert_from_csr(csr_base64: &String, ca_cert: &X509, ca_pkey: &PKey<Priv
     };
     // Build certificate
     let cert_final = cert_builder.build();
-    
-    return (to_base64(&cert_final.to_pem().unwrap()), Status::SUCCESS, Status::SUCCESS.to_string());
+
+    return (
+        to_base64(&cert_final.to_pem().unwrap()),
+        Status::SUCCESS,
+        Status::SUCCESS.to_string(),
+    );
 }
 
 /// Function for `create-from-csr` Warp route
-/// 
+///
 /// # Arguments
 /// * `item`
 /// * `ca_cert`
 /// * `ca_pkey`
-/// 
+///
 /// # Returns
 /// * `SignRep`
 async fn create_from_csr(
@@ -178,45 +188,42 @@ async fn create_from_csr(
     let cert_rep = SignRep {
         signed_cert_base64: cert_tuple.0,
         status: cert_tuple.1,
-        status_message: cert_tuple.2
+        status_message: cert_tuple.2,
     };
     Ok(warp::reply::json(&cert_rep))
 }
 
 /// Function for `get-ca-cert` Warp route
-/// 
+///
 /// # Arguments
 /// * `ca_cert`
-/// 
+///
 /// # Returns
 /// * `CaCertRep`
-async fn get_ca_cert(
-    ca_cert: X509,
-) -> Result<impl warp::Reply, warp::Rejection> {
+async fn get_ca_cert(ca_cert: X509) -> Result<impl warp::Reply, warp::Rejection> {
     let ca_cert_rep = CaCertRep {
         ca_cert_base64: to_base64(&ca_cert.to_pem().unwrap()),
         status: Status::SUCCESS,
-        status_message: Status::SUCCESS.to_string()
+        status_message: Status::SUCCESS.to_string(),
     };
     Ok(warp::reply::json(&ca_cert_rep))
 }
 
-
 /// rust-toy-ca uses `warp` and `openssl` to sign certificates from CSR
-/// 
+///
 /// To run from CLI, you will need to pass the following arguments:
 /// * `--ca-cert-file`: path for PEM certificate
 /// * `--ca-pkey-file`: path for PKCS8 key
-/// 
+///
 /// # Examples
 /// ```
 /// $ cargo run -- --ca-cert-file test_helpers/ca.crt --ca-pkey-file test_helpers/ca.key
 /// ```
-/// 
+///
 /// ```
 /// $ ./rust-toy-ca --ca-cert-file test_helpers/ca.crt --ca-pkey-file test_helpers/ca.key
 /// ```
-/// 
+///
 #[tokio::main]
 async fn main() {
     // Load logging configuration
@@ -224,9 +231,12 @@ async fn main() {
 
     // Load CLI args
     let args = Args::parse();
-    
+
     // Listen address:port
-    let listen_addr: SocketAddr = args.listen.parse().expect("Could not parse passed IP:Port !!!");
+    let listen_addr: SocketAddr = args
+        .listen
+        .parse()
+        .expect("Could not parse passed IP:Port !!!");
 
     // Load certificates and keys
     // Make a clone for warp
@@ -252,7 +262,9 @@ async fn main() {
         .and(ca_cert_w.clone())
         .and(ca_pkey_w.clone())
         .and_then(create_from_csr);
-    let routes = warp::any().and(route_get_ca_cert.or(route_create_from_csr)).with(warp::log("stdout"));
+    let routes = warp::any()
+        .and(route_get_ca_cert.or(route_create_from_csr))
+        .with(warp::log("stdout"));
 
     // Start server
     info!("rust-toy-ca running...");
